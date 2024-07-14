7f1f67ee8639621389f140f7918a1042c8ceb351
victor <demciuc.victor@gmail.com>
2024-07-14 16:28:59 +0200

    Initial commit

---
+++ src/main/java/nl/learn/api_ca/ApiCodingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package nl.learn.api_ca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCodingAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCodingAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/learn/api_ca/ApiCodingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.learn.api_ca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCodingAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
